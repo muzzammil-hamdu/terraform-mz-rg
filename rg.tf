95242632b3a6e0701a620d0e979481e32ac9025f
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-15 00:37:21 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "winvm-vnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.10.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "winvm-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.10.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
