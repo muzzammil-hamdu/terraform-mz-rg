a6df3d00bba43ac0764a5880ff78321a06992dc2
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-15 03:14:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,75 +1,109 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "trm-mz"
-
-    workspaces {
-      name = "terraform-mz-rg"
-    }
-  }
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.100"
     }
   }
-}
-provider "azurerm" {
-  features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-}
-
-resource "azurerm_resource_group" "main" {
-  name     = "my-winvm-rg"
-  location = "UK South"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "winvm-vnet"
-  address_space       = ["10.10.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "winvm-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.1.0/24"]
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "winvm-ip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "winvm-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "winvm-ip-config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "trm-mz"
+    workspaces {
+      name = "terraform-mz-rg"
+    }
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terrarg6"
+  location = "uk south"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-win"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-win"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "winvm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "winvm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-WinRM"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# NIC
+resource "azurerm_network_interface" "nic" {
+  name                = "winvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Attach NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Windows VM
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = "my-winvm"
-  resource_group_name   = azurerm_resource_group.main.name
-  location              = azurerm_resource_group.main.location
-  size                  = "Standard_D4ds_v5"
-  admin_username        = "mujju"
-  admin_password        = "Muzzammil@752"
+  name                = "winhamd"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D4s_v3"
+  admin_username      = "mujju"
+  admin_password      = "Muzzammil@752"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
@@ -80,11 +114,32 @@ resource "azurerm_windows_virtual_machine" "vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-datacenter"
+    sku       = "2022-Datacenter"
     version   = "latest"
   }
 }
 
-output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.ip_address
+# Enable WinRM
+resource "azurerm_virtual_machine_extension" "winrm" {
+  name                 = "enable-winrm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -Command \\
+      \"winrm quickconfig -q; \\
+      winrm set winrm/config/winrs @{MaxMemoryPerShellMB='512'}; \\
+      winrm set winrm/config @{MaxTimeoutms='1800000'}; \\
+      winrm set winrm/config/service @{AllowUnencrypted='true'}; \\
+      winrm set winrm/config/service/auth @{Basic='true'}; \\
+      netsh advfirewall firewall add rule name='WinRM HTTP' dir=in action=allow protocol=TCP localport=5985\""
+    }
+  SETTINGS
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
