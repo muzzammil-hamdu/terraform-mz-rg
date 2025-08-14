ef22274ec4168569ebf18932602e087f4f4ed39f
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-15 02:10:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,6 +68,30 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "winvm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG to NIC
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
 # Windows VM
 resource "azurerm_windows_virtual_machine" "vm" {
