82eb79cbc09bc6b9acb086440912e44bc49c1173
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-15 01:23:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,96 +1,73 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "trm-mz"
-    workspaces {
-      name = "terraform-mz-rg"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  client_id       = "52883a55-f8e4-450c-a9a2-c98920f818fc"
+  client_secret   = "z1v8Q~Hv7~QBbbb9Akoew9lbauFR5b74oCOmGalC"
+  tenant_id       = "66573a45-6f85-4878-bebc-e0bc24647836"
+  subscription_id = "5d1b700e-5c37-4a48-a430-e148b56e5404"
 }
 
-# ---------------- Network & VM ----------------
-
+# Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "my-winvm-rg"
+  location = "UK South"
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vm_name}-vnet"
+  name                = "winvm-vnet"
   address_space       = ["10.10.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.vm_name}-subnet"
+  name                 = "winvm-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.vm_name}-nsg"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "allow_rdp"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
+# Public IP (Static)
 resource "azurerm_public_ip" "public_ip" {
-  name                = "${var.vm_name}-pip"
+  name                = "winvm-ip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
+# NIC
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
+  name                = "winvm-nic"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "ipconfig1"
+    name                          = "winvm-ip-config"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
+# Windows VM
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = var.vm_name
+  name                  = "my-winvm"
   resource_group_name   = azurerm_resource_group.main.name
   location              = azurerm_resource_group.main.location
-  size                  = var.vm_size
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
+  size                  = "Standard_D4ds_v5"
+  admin_username        = "mujju"
+  admin_password        = "Muzzammil@752"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
@@ -104,12 +81,9 @@ resource "azurerm_windows_virtual_machine" "vm" {
     sku       = "2022-datacenter"
     version   = "latest"
   }
-
-  provision_vm_agent = true
 }
 
-# ---------------- Software install via Custom Script Extension ----------------
-
+# Custom Script Extension - Install Software
 resource "azurerm_virtual_machine_extension" "install_software" {
   name                 = "install-software"
   virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
@@ -117,15 +91,17 @@ resource "azurerm_virtual_machine_extension" "install_software" {
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
-  settings = jsonencode({
-    fileUris         = ["https://<your-storage-account>.blob.core.windows.net/scripts/install_software.ps1"]
-    commandToExecute = "powershell -ExecutionPolicy Bypass -File install_software.ps1"
-  })
-
-  depends_on = [azurerm_windows_virtual_machine.vm]
+  settings = <<SETTINGS
+  {
+    "fileUris": [
+      "https://raw.githubusercontent.com/muzzammilhussain/azure-scripts/main/install.ps1"
+    ],
+    "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File install.ps1"
+  }
+  SETTINGS
 }
 
+# Output Public IP
 output "public_ip_address" {
-  description = "Public IP of the Windows VM"
-  value       = azurerm_public_ip.public_ip.ip_address
+  value = azurerm_public_ip.public_ip.ip_address
 }
