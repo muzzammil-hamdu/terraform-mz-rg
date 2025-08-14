e1271b3b7133300bbf64d359abb976761c4838be
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-15 01:16:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-pip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
