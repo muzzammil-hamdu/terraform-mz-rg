1cad56b6a7f28fc773bf44d69d3d1d931458188d
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-15 01:38:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,68 +1,78 @@
 terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "trm-mz"
+
+    workspaces {
+      name = "terraform-mz-rg"
+    }
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~>3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
-  client_id       = "52883a55-f8e4-450c-a9a2-c98920f818fc"
-  client_secret   = "z1v8Q~Hv7~QBbbb9Akoew9lbauFR5b74oCOmGalC"
-  tenant_id       = "66573a45-6f85-4878-bebc-e0bc24647836"
-  subscription_id = "5d1b700e-5c37-4a48-a430-e148b56e5404"
 }
 
-resource "azurerm_resource_group" "main" {
+# Resource group
+resource "azurerm_resource_group" "rg" {
   name     = "my-winvm-rg"
-  location = "UK South"
+  location = "East US"
 }
 
+# Virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "winvm-vnet"
-  address_space       = ["10.10.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "winvm-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Public IP (Static, Standard SKU)
 resource "azurerm_public_ip" "public_ip" {
-  name                = "winvm-ip"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"   # Fixed Public IP
+  name                = "winvm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
+# Network Interface
 resource "azurerm_network_interface" "nic" {
   name                = "winvm-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "winvm-ip-config"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic" # Azure handles it
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
+# Windows VM
 resource "azurerm_windows_virtual_machine" "vm" {
   name                  = "my-winvm"
-  resource_group_name   = azurerm_resource_group.main.name
-  location              = azurerm_resource_group.main.location
-  size                  = "Standard_D4ds_v5"
-  admin_username        = "mujju"
-  admin_password        = "Muzzammil@752"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_D2s_v3"
+  admin_username        = "azureuser"
+  admin_password        = "P@ssw0rd12345!"
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
@@ -73,12 +83,12 @@ resource "azurerm_windows_virtual_machine" "vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-datacenter"
+    sku       = "2022-Datacenter"
     version   = "latest"
   }
 }
 
-# Install Java, Teams, Chrome via Custom Script Extension
+# Install Java, Teams, and Chrome
 resource "azurerm_virtual_machine_extension" "install_software" {
   name                 = "install-software"
   virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
@@ -86,17 +96,7 @@ resource "azurerm_virtual_machine_extension" "install_software" {
   type                 = "CustomScriptExtension"
   type_handler_version = "1.10"
 
-  settings = <<SETTINGS
-{
-  "commandToExecute": "powershell -ExecutionPolicy Unrestricted -Command \"
-    Invoke-WebRequest -Uri https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe -OutFile C:\\\\Temp\\\\java.exe; Start-Process C:\\\\Temp\\\\java.exe -ArgumentList '/s' -Wait;
-    Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/p/ -OutFile C:\\\\Temp\\\\teams.exe; Start-Process C:\\\\Temp\\\\teams.exe -ArgumentList '/silent' -Wait;
-    Invoke-WebRequest -Uri https://dl.google.com/chrome/install/latest/chrome_installer.exe -OutFile C:\\\\Temp\\\\chrome.exe; Start-Process C:\\\\Temp\\\\chrome.exe -ArgumentList '/silent /install' -Wait;
-  \""
-}
-SETTINGS
-}
-
-output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.ip_address
+  settings = jsonencode({
+    commandToExecute = "powershell -ExecutionPolicy Unrestricted -Command \"New-Item -ItemType Directory -Path C:\\Temp -Force; Invoke-WebRequest -Uri https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.exe -OutFile C:\\Temp\\java.exe; Start-Process C:\\Temp\\java.exe -ArgumentList '/s' -Wait; Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/p/?linkid=2139997 -OutFile C:\\Temp\\teams.exe; Start-Process C:\\Temp\\teams.exe -ArgumentList '/silent' -Wait; Invoke-WebRequest -Uri https://dl.google.com/chrome/install/latest/chrome_installer.exe -OutFile C:\\Temp\\chrome.exe; Start-Process C:\\Temp\\chrome.exe -ArgumentList '/silent /install' -Wait\""
+  })
 }
